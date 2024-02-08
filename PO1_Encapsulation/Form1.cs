40ef4e504c5ad5d2432c9644f16f4007895d289d
Jesse Veilleux <146979972+Uberaus@users.noreply.github.com>
2024-02-08 11:40:52 -0500

    FirstPart

--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -2,24 +2,47 @@ namespace PO1_Encapsulation
 {
     public partial class Form1 : Form
     {
+        Personnage joueur = new();
+        Personnage dragon = new();
         public Form1()
         {
             InitializeComponent();
+            joueur.PointsDeVie = 10;
+            joueur.PointsDePuissance = 1;
+
+            dragon.PointsDeVie = 30;
+            dragon.PointsDePuissance = 3;
+
+
+        }
+        private void ActualiserLabel()
+        {
+            lbl_pointsVieJoueur.Text = "vie du joueur : " + joueur.PointsDeVie.ToString();
+            lbl_pointsVieDragon.Text = "vie du dragon : " + dragon.PointsDeVie.ToString();
+            lbl_puissanceAttaqueJoueur.Text = "Attaque du joueur : " + joueur.PointsDePuissance.ToString();
+            lbl_puissanceAttaqueDragon.Text = "Attaque du dragon : " + dragon.PointsDePuissance.ToString();
+
         }
 
         private void btn_attaquer_Click(object sender, EventArgs e)
         {
-
+            dragon.PointsDeVie -= joueur.PointsDePuissance;
+            ActualiserLabel();
         }
 
         private void btn_seSoigner_Click(object sender, EventArgs e)
         {
-
+            ActualiserLabel();
         }
 
         private void btn_puissanceAttaque_Click(object sender, EventArgs e)
         {
+            ActualiserLabel();
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ActualiserLabel();
         }
     }
 }
---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    public class Personnage
+    {
+        string name;
+        int pointsDeVie;
+        int pointsDePuissance;
+
+        public string Name { get => name;  }
+        public int PointsDeVie { get => pointsDeVie;
+            set
+            {
+
+                if (value < 0)
+                {
+                    pointsDeVie = 0;
+                }
+                else
+                {
+                    pointsDeVie = value;
+                }
+            }
+        }
+        public int PointsDePuissance { get => pointsDePuissance; set => pointsDePuissance = value; }
+
+        public bool EstEnVie
+        {
+            get
+            {
+                if(pointsDeVie > 0)
+                {
+                    return true;    
+                }
+                return false;   
+            }
+        }
+    }
+}
